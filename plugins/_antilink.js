dfbe224c7000e7941f2f48ed73e1baf26d881fda
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-08 19:19:13 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -7,13 +7,13 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    await m.reply(`*「 ANTI LINK 」*\n\nTerdeteksi *${await this.getName(m.sender)}* kamu telah mengirim link group!\n\nMaaf Kamu akan dikick dari grup ini byee!`)
-    if (isAdmin) return m.reply('*Eh maap kamu admin, kamu gk bakal dikick. hehe..*')
-    if (!isBotAdmin) return m.reply('*Bot bukan admin, mana bisa kick orang _-*')
+    await m.reply(`*「 ANTI LINK 」*\n\nDetectado *${await this.getName(m.sender)}* has enviado el enlace del grupo!\n\nLo siento, serás expulsado de este grupo, adiós!`)
+    if (isAdmin) return m.reply('*Lo siento, eres administrador, no te expulsaran. ja ja..*')
+    if (!isBotAdmin) return m.reply('*El bot no es administrador, ¿cómo puede sacar a la gente? _-*')
     let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
     let isLinkThisGc = new RegExp(linkGC, 'i')
     let isgclink = isLinkThisGc.test(m.text)
-    if (isgclink) return m.reply('*Lol ngirim link group sendiri :v*')
+    if (isgclink) return m.reply('*Lol envía tu propio enlace de grupo :v*')
     await this.groupRemove(m.chat, [m.sender])
   }
   return true

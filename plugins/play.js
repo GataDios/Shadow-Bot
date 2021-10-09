ee637c2b0abc0fa146f7cc505e0b242f5d35b9f8
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-08 20:44:15 -0500

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -4,26 +4,26 @@ let yts = require('yt-search')
 const { servers, yta, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, command, text, isPrems, isOwner, DevMode }) => {
   conn.play = conn.play ? conn.play : {}
-  if (m.chat in conn.play) throw 'Masih ada yang sedang mencari di youtube\ndi chat ini... tunggu sampai selesai'
+  if (m.chat in conn.play) throw 'Alguien todavía está buscando en YouTube\nen este chat... espera hasta que termine'
   else conn.play[m.chat] = true
   try {
       try {
-          if (!text) throw 'Cari apa?'
+          if (!text) throw 'Qué estás buscando?'
           let results = await yts(text)
           let vid = results.all.find(video => video.seconds < 3600)
-          if (!vid) throw 'Video/Audio Tidak ditemukan'
+          if (!vid) throw 'Video/Audio No encontrado'
           let { dl_link, thumb, title, filesize, filesizeF} = await (/2$/.test(command) ? ytv : yta)(vid.url, 'id4')
           let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
           conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+*Titulo:* ${title}
+*Tamaño del archivo:* ${filesizeF}
+*Fuente:* ${vid.url}
+*${isLimit ? 'Usar ': ''}Link:* ${dl_link}
 `.trim(), m)
           if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
+*Titulo:* ${title}
+*Tamaño del archivo:* ${filesizeF}
+*Fuente:* ${vid.url}
 `.trim(), m)
       } catch (e) {
           console.log(e)

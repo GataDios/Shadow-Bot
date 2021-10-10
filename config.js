9034b16cdb10ab936367c3bf74f3d63229ad2327
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-10 12:17:30 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://ch
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
   '19527773705': { // put your number here
-    name: 'Bruno, dueño de: The Shadow Brokers - Bot',
+    name: 'Bruno Sobrino',
     isDev: true, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator

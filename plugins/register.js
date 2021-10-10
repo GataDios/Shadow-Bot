a8a8536605f076f65b7abebe72ac9264cc30cc1b
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-10 12:21:39 -0500

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -2,11 +2,11 @@ const { createHash } = require('crypto')
 let Reg = /(.*)([.|])([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix }) {
   let user = global.DATABASE._data.users[m.sender]
-  if (user.registered === true) throw `Anda sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
-  if (!Reg.test(text)) throw `Foemat salah\n*${usedPrefix}daftar <nama>.umur>*`
+  if (user.registered === true) throw `Ya estás registrado\nQuieres volver a registrarte? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
+  if (!Reg.test(text)) throw `Formato incorrecto\n*${usedPrefix}daftar <nombre>.edad>*`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
-  if (!age) throw 'Umur tidak boleh kosong (Angka)'
+  if (!name) throw 'El nombre no puede estar vacío (Alphanumeric)'
+  if (!age) throw 'La edad no puede estar vacía (Angka)'
   user.name = name
   user.age = parseInt(age)
   user.regTime = + new Date
@@ -16,8 +16,8 @@ let handler = async function (m, { text, usedPrefix }) {
 Daftar berhasil!
 
 ╭─「 Info 」
-│ Nama: ${name}
-│ Umur: ${age}thn
+│ Nombre: ${name}
+│ Edad: ${age}thn
 │ SN: ${sn}
 ╰────
 `.trim())

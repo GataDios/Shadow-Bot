78900833d514b1389115f85e483ed5463bac89aa
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-10 23:30:41 -0500

    Update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -6,12 +6,12 @@ let handler = async (m, { conn }) => {
     let _timers = (86400000 - __timers)
     let timers = clockString(_timers) 
     if (new Date - user.lastclaim > 86400000) {
-        conn.reply(m.chat, `Anda sudah mengklaim dan mendapatkan 1000 💵money dan 1 potion`, m)
+        conn.reply(m.chat, `Ya reclamaste y obtuviste 1000 💵 dinero y 1 poción`, m)
         global.DATABASE._data.users[m.sender].money += 1000
         global.DATABASE._data.users[m.sender].potion += 1
         global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
     } else {
-        let buttons = button(`silahkan tunggu *🕒${timers}* lagi untuk bisa mengclaim lagi`, user)
+        let buttons = button(`por favor espere *🕒${timers}* para poder reclamar nuevamente`, user)
         conn.sendMessage(m.chat, buttons, MessageType.buttonsMessage, { quoted: m })
     }
 }

0d37fd92c7d097413afeef2d7f905720def02a4e
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-11 20:51:09 -0500

    Update family100.js

--- plugins/family100.js
+++ plugins/family100.js
@@ -20,7 +20,7 @@ async function handler(m) {
     this.game = this.game ? this.game : {}
     let id = 'family100_' + m.chat
     if (id in this.game) {
-        this.reply(m.chat, 'Masih ada kuis yang belum terjawab di chat ini', this.game[id].msg)
+        this.reply(m.chat, 'Todavía hay cuestionarios sin respuesta en este chat', this.game[id].msg)
         throw false
     }
 
@@ -44,7 +44,7 @@ Terdapat *${json.jawaban.length}* jawaban${json.jawaban.find(v => v.includes(' '
 (beberapa jawaban terdapat spasi)
 `: ''}
 
-+${winScore} XP tiap jawaban benar
++${winScore} XP cada respuesta correcta
     `.trim()
     this.game[id] = {
         id,

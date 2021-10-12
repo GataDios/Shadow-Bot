bc5afa987a1bc4a3dc0bc13ff1780bc641fa4866
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-12 06:58:28 -0500

    Update caklontong_answer.js

--- plugins/caklontong_answer.js
+++ plugins/caklontong_answer.js
@@ -1,9 +1,9 @@
 let handler = m => m
 handler.before = async function (m) {
   let id = m.chat
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*hint caklontong/i.test(m.quoted.text)) return
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Escribe.*hint caklontong/i.test(m.quoted.text)) return
   conn.caklontong = conn.caklontong ? conn.caklontong : {}
-  if (!(id in conn.caklontong)) throw 'Soal itu telah berakhir!'
+  if (!(id in conn.caklontong)) throw 'El asunto ha terminado!'
   if (m.quoted.id == conn.caklontong[id][0].id) {
     let json = JSON.parse(JSON.stringify(conn.caklontong[id][1]))
     // m.reply(JSON.stringify(json, null, '\t'))
@@ -13,7 +13,7 @@ handler.before = async function (m) {
       clearTimeout(conn.caklontong[id][3])
       delete conn.caklontong[id]
     } else if (m.text.toLowerCase().endsWith(json.result.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
-    else m.reply(`*Salah!*`)
+    else m.reply(`*Incorrecto!*`)
   }
 }
 handler.exp = 0

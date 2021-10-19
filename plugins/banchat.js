cb4b5ae637f5e0e03f2a8edfab005f67b3af6bc0
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-19 08:09:03 -0500

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, participants }) => {
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
-    if (!(m.chat in global.DATABASE._data.chats)) return m.reply('Chat ini tidak terdaftar dalam DATABASE!')
+    if (!(m.chat in global.DATABASE._data.chats)) return m.reply('Este chat no está registrado en la BASE DE DATOS!')
     let chat = global.DATABASE._data.chats[m.chat]
-    if (chat.isBanned) return m.reply('Chat ini sudah Terbanned!')
+    if (chat.isBanned) return m.reply('Este chat ha sido baneado por imcumplir alguna regla!')
     chat.isBanned = true
-    m.reply('Done!')
-  // } else m.reply('Ada nomor host disini...')
+    m.reply('Listo!')
+  // } else m.reply('Aquí hay un número de un host...')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']

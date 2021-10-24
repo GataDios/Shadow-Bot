ad2548ce370b115642df49515402727ae6ea345d
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-24 10:54:08 -0500

    Update apakah2.js

--- plugins/apakah2.js
+++ plugins/apakah2.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, command, text }) => {
   conn.reply(m.chat, `
 *Pertanyaan:* ${command} ${text}
-*Jawaban:* ${pickRandom(['Ya','Mungkin iya','Mungkin','Mungkin tidak','Tidak','Tidak mungkin'])}
+*Jawaban:* ${pickRandom(['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible'])}
 `.trim(), m, m.mentionedJid ? {
     contextInfo: {
       mentionedJid: m.mentionedJid

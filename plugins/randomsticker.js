a300bef87c63af8f4718d5470ad5b2446f10c9f2
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-25 14:39:44 -0500

    Update randomsticker.js

--- plugins/randomsticker.js
+++ plugins/randomsticker.js
@@ -3,9 +3,9 @@ const axios = require('axios')
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/sticker')
             conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
   }
-handler.help = ['stickerrandom']
+handler.help = ['imagenrandom']
 handler.tags = ['images']
-handler.command = /^(stickerrandom)$/i
+handler.command = /^(imagenrandom)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false

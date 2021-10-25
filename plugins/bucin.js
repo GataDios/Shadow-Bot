68cccf063e9fea3bc4c551a2596142282647e42e
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-25 12:07:13 -0500

    Update bucin.js

--- plugins/bucin.js
+++ plugins/bucin.js
@@ -2,7 +2,7 @@ let handler  = async (m, { conn }) => {
   conn.reply(m.chat,`“${pickRandom(global.bucin)}”`, m)
 }
 handler.help = ['reto']
-handler.tags = ['quotes']
+handler.tags = ['games']
 handler.command = /^(reto)$/i
 handler.owner = false
 handler.mods = false

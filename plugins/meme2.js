15aa6282c92346d121fe0b69e3a9579de9e429a7
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-25 14:42:37 -0500

    Update meme2.js

--- plugins/meme2.js
+++ plugins/meme2.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
  let handler = async(m, { conn }) => {
-let les = await axios.get('https://meme-api.herokuapp.com/gimme/memees')
+let les = await axios.get('https://meme-api.herokuapp.com/gimme/spanishmeme')
             conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
   }
 handler.help = ['meme2']

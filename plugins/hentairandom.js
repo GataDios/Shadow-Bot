6a295be38e157a0313a1fedd72b04936714f7976
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-25 14:46:10 -0500

    Update and rename meme2.js to hentairandom.js

--- plugins/hentairandom.js
+++ plugins/hentairandom.js
@@ -1,11 +1,11 @@
 const axios = require('axios')
  let handler = async(m, { conn }) => {
-let les = await axios.get('https://meme-api.herokuapp.com/gimme/spanishmeme')
+let les = await axios.get('https://meme-api.herokuapp.com/gimme/hentai')
             conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
   }
-handler.help = ['meme2']
+handler.help = ['hentai']
 handler.tags = ['images']
-handler.command = /^(meme2)$/i
+handler.command = /^(hentai)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false

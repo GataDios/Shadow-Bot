66e502c81ccc39ebc76698508624886e43456d54
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-25 06:42:53 -0500

    Update Créditosyagradecimientos.js

--- plugins/Créditosyagradecimientos.js
+++ plugins/Créditosyagradecimientos.js
@@ -1,13 +1,21 @@
 let handler = async m => m.reply(`
-    *_The Shadow Brokers - Bot_*
-    
+*_「 THE SHADOW BROKERS - BOT 」_*
+
 Base: https://github.com/BochilGaming/games-wabot
 
 GitHub editada: https://github.com/TheShadowBrokers1/games-wabot
 
 Créditos del diseño del menu: 💮Oreki-san💮
 Numero: +51 936 366 761
-Link directo: https://api.whatsapp.com/send?phone=51936366761_
+Link directo: https://api.whatsapp.com/send?phone=51936366761
+
+Agradecemos a Estefanía y a Maximiliano por ser moderadores del Bot 
+
+Agradecemos a todos los usuarios activos del Bot
+
+Agradecemos a todas las personas que intervinieron en la edicion o aprobacion del Bot y sus comandos
+
+*_「 𝙏͚͜͝͠𝙝͚͜͝͠𝙚͚͜͝͠ ͚͜͝͠𝙎͚͜͝͠𝙝͚͜͝͠𝙖͚͜͝͠𝙙͚͜͝͠𝙤͚͜͝͠𝙬͚͜͝͠ ͚͜͝͠𝘽͚͜͝͠𝙧͚͜͝͠𝙤͚͜͝͠𝙠͚͜͝͠𝙚͚͜͝͠𝙧͚͜͝͠𝙨͚͜͝͠ ͚͜͝͠-͚͜͝͠ ͚͜͝͠𝘽͚͜͝͠𝙤͚͜͝͠𝙩͚͜͝͠ ͚͜͝͠  」_*
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['creditosyagradecimientos']
 handler.tags = ['info']

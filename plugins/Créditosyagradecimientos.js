e4a96192681e18c0b5410945d2d3ae1338dd015e
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-25 06:34:15 -0500

    Update Créditosyagradecimientos.js

--- plugins/Créditosyagradecimientos.js
+++ plugins/Créditosyagradecimientos.js
@@ -1,6 +1,4 @@
-let levelling = require('../lib/levelling')
-let handler = async (m, { conn, usedPrefix }) => {
-    let str = `
+let handler = async m => m.reply(`
     *_The Shadow Brokers - Bot_*
     
 Base: https://github.com/BochilGaming/games-wabot
@@ -10,12 +8,9 @@ GitHub editada: https://github.com/TheShadowBrokers1/games-wabot
 Créditos del diseño del menu: 💮Oreki-san💮
 Numero: +51 936 366 761
 Link directo: https://api.whatsapp.com/send?phone=51936366761_
-    conn.reply(m.chat, str, m)
-}
+`.trim()) // Tambah sendiri kalo mau
 handler.help = ['creditosyagradecimientos']
 handler.tags = ['info']
-handler.command = /^(creditosyagradecimientos)$/i
-module.exports = handler
+handler.command = /^creditosyagradecimientos$/i
 
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
+module.exports = handler

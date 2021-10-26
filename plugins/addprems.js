826b0783844f1553e04a387be18a92ab09d34eef
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-26 15:33:43 -0500

    Update addprems.js

--- plugins/addprems.js
+++ plugins/addprems.js
@@ -8,7 +8,7 @@ let who
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].premium = true
   var nomor = m.sender
-    m.reply(`*✅ Listo, te haz agregado correctamente ✅*\n\n*Numero:* wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*Expira en:* 30 Dias\n\n*Gracias por agregarte a usuario _PREMIUM_!*`)
+    m.reply(`*✅ Listo, te haz agregado correctamente ✅*\n\n*Numero:* wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*Expira en:* 30 Dias\n\n*Gracias por agregarte a usuario premium!*\n*Te ganaste un lugar en mi corazon <3*`)
   // } else m.reply('Ada nomor host disini...')
 }
 handler.help = ['addprems <nomor>']

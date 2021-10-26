847b833430a3b09ddba33dbca25a61f4cb3d5ca9
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-26 10:24:02 -0500

    Update Random K-pop.js

--- plugins/Random K-pop.js
+++ plugins/Random K-pop.js
@@ -5,7 +5,7 @@ let les = await axios.get('https://meme-api.herokuapp.com/gimme/kpop')
   }
 handler.help = ['randomkpop']
 handler.tags = ['images']
-handler.command = /^(lesbian)$/i
+handler.command = /^(randomkpop)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false

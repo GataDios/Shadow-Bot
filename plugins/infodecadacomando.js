357c45115fe9e5e7562264ba07df2dcf87e7f7ad
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-26 14:15:27 -0500

    Update infodecadacomando.js

--- plugins/infodecadacomando.js
+++ plugins/infodecadacomando.js
@@ -7,7 +7,7 @@ let handler  = async (m, { conn, usedPrefix }) => {
   let uptime = clockString(_uptime) 
   let totalreg = Object.keys(global.DATABASE._data.users).length
   let old = Math.round(performance.now())
-  await m.reply('*LEA DETALLADAMENTE CADA COMANDO Y SU EXPLICACION!!*')
+  await m.reply('*GRACIAS POR SOLICITAR EL MENU DE EXPLICACIONES/nLEA DETALLADAMENTE CADA COMANDO Y SU EXPLICACION!! /// ALGUNOS COMANDOS NO APARECEN AQUI, SE ESTA TRABAJANDO EN AGREGARLOS*')
   let neww = Math.round(performance.now())
   conn.reply(m.chat, `
 *_LISTA DE COMANDOS CON SU EXPLICACION:_*

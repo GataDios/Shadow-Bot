d5a62fea8bcf6c76ebedd3387b7207b2a2d0422b
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-26 14:05:21 -0500

    Create A_menu1_edit.js

---
+++ plugins/A_menu1_edit.js
@@ -0,0 +1,198 @@
+let handler = async m => m.reply(`
+「 THE SHADOW BROKERS - BOT 」
+
+Información del Bot
+- Propietario: Bruno sobrino 
+- Numero del propietario: +1 (952) 522-0880
+- Link directo: https://api.whatsapp.com/send?phone=19525220880
+Moderadora 1: Estefanía Gutierrez 
+Moderador 2: Maximiliano de la cruz 
+ 
+Reglas e información sobre el Bot: https://the-shadow-brokers-bot-info.blogspot.com/2021/10/the-shadow-brokers-bot.html
+⚠️-EL DESCONOCIMIENTO DE LO MENCIONADO EN ESE ARTICULO NO EXIME A LA PERSONA DE LAS REGLAS Y SANCIONES QUE DE ELLA EMANAN-⚠️
+
+Grupo oficial del Bot: https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ
+
+Prefijo del Bot: MULTI-PREFIJO
+(#, $, ?, !, /, .)
+
+<OTROS MENUS>
+
+° ඬ⃟ℹ️ #Infodecadacomando
+° ඬ⃟ℹ️ #reglasdelbot
+° ඬ⃟ℹ️ #creditosyagradecimientos
+
+   <JUEGOS/>
+
+° ඬ⃟🕹 #howgay @tag / nombre
+° ඬ⃟🕹 #howlesbi @tag / nombre
+° ඬ⃟🕹 #Formarpareja
+° ඬ⃟🕹 #slots cantidad
+
+<ECONOMIA/> 
+
+° ඬ⃟💵 #monthly
+° ඬ⃟💵 #Claim
+° ඬ⃟💵 #weekly
+° ඬ⃟💵 #billetera
+
+   <DESCARGAS/>
+
+° ඬ⃟📥 #google  texto
+° ඬ⃟📥 #googlef texto
+° ඬ⃟📥 #image  texto
+° ඬ⃟📥 #gimage  texto
+° ඬ⃟📥 #ytsearch  texto
+° ඬ⃟📥 #ytmp4  link yt
+° ඬ⃟📥 #ytmp3  link yt
+° ඬ⃟📥 #play  titulo
+° ඬ⃟📥 #play2  titulo del video
+° ඬ⃟📥 #lirik  nombredelacanción
+ 
+<GESTION DE GRUPOS/> 
+
+° ඬ⃟💎 #admin @tag
+° ඬ⃟💎 #kick @tag
+° ඬ⃟💎 #add  número
+° ඬ⃟💎 #group open/close
+° ඬ⃟💎 #Enable welcome
+° ඬ⃟💎 #Disable welcome
+° ඬ⃟💎 #group open/close
+° ඬ⃟💎 #listonline
+° ඬ⃟💎 #enable antilink
+° ඬ⃟💎 #disable antilink 
+° ඬ⃟💎 #link
+° ඬ⃟💎 #linkgroup
+° ඬ⃟💎 #hidetag texto
+° ඬ⃟💎 #Setname Nuevo nombre del grupo
+
+<CREADORES/>
+
+° ඬ⃟🧧 #Style texto
+° ඬ⃟🧧 #ytcomment texto
+° ඬ⃟🧧 #hornycard texto
+° ඬ⃟🧧 #S
+° ඬ⃟🧧 #sticker*
+° ඬ⃟🧧 #swm Link de la imagen
+° ඬ⃟🧧 #ttp texto
+° ඬ⃟🧧 #attp texto
+° ඬ⃟🧧 #attp2 texto
+° ඬ⃟🧧 #wasted
+° ඬ⃟🧧 #rainbow
+° ඬ⃟🧧 #circle
+° ඬ⃟🧧 #tomp3
+° ඬ⃟🧧 #toimg
+
+<RANDOM/>
+
+° ඬ⃟👾 #meme
+° ඬ⃟👾 #meme2
+° ඬ⃟👾 #meme3
+° ඬ⃟👾 #cat
+° ඬ⃟👾 #dog
+° ඬ⃟👾 #pikachu
+° ඬ⃟👾 #waifu
+° ඬ⃟👾 #apakah pregunta
+° ඬ⃟👾 #blackpink
+° ඬ⃟👾 #reto
+° ඬ⃟👾 #imagenrandom
+° ඬ⃟👾 #randomkpop
+
+<COMANDOS +18/>
+​​​
+° ඬ⃟🔞 #Image Hentai sin censura
+° ඬ⃟🔞 #Image Hentai
+° ඬ⃟🔞 #gimage Hentai
+° ඬ⃟🔞 #image yaoi
+° ඬ⃟🔞 #Image loli
+° ඬ⃟🔞 #Image Porno
+° ඬ⃟🔞 #Image Tetas
+° ඬ⃟🔞 #Image chico desnudo
+° ඬ⃟🔞 #Image Pene
+° ඬ⃟🔞 #Image XXX
+° ඬ⃟🔞 #Image Teen desnuda
+- Si la imagen se envía en formato de archivo reintentar nuevamente
+
+<COMANDOS +18/>
+- Usuarios premium 
+​​​
+° ඬ⃟🔞 #lesbian
+° ඬ⃟🔞 #porno
+° ඬ⃟🔞 #randomnsfw
+° ඬ⃟🔞 #pornogif
+° ඬ⃟🔞 #hentai
+
+<EFECTOS PARA NOTAS DE VOZ O AUDIOS/>
+​​​
+° ඬ⃟🎤 #bass
+° ඬ⃟🎤 #deep
+° ඬ⃟🎤 #earrape
+° ඬ⃟🎤 #fast
+° ඬ⃟🎤 #fat
+° ඬ⃟🎤 #nightcore
+° ඬ⃟🎤 #reverse
+° ඬ⃟🎤 #robot
+° ඬ⃟🎤 #slow
+° ඬ⃟🎤 #smooth
+
+<EXTRAS/>
+
+° ඬ⃟🐺 #Join link del grupo
+° ඬ⃟🐺 #daftar nombre.edad
+° ඬ⃟🐺 #profile
+° ඬ⃟🐺 #Simih texto
+° ඬ⃟🐺 #afk motivo
+° ඬ⃟🐺 #readmore texto1| texto2
+° ඬ⃟🐺 #qr texto
+° ඬ⃟🐺 #Calc expresión matemática 
+° ඬ⃟🐺 #Zodiac AAAA MM DD
+° ඬ⃟🐺 #Scan numero con prefijo del pais
+
+<CHAT ANONIMO/>
+
+° ඬ⃟📳 #start
+° ඬ⃟📳 #next
+° ඬ⃟📳 #leave
+
+<CONVIERTETE EN BOT/>
+
+° ඬ⃟🏷 #stop
+° ඬ⃟🏷 #jadibot
+
+<CREA TU NUMERO VIRTUAL E INSTALA UN BOT EN EL/>
+
+° ඬ⃟🔥 #Tutorial
+
+<PROPIETARIO Y MODERADORES/>
+
+° ඬ⃟🤖 #banuser @tag
+° ඬ⃟🤖 #banchat
+° ඬ⃟🤖 #setmenu
+° ඬ⃟🤖 #enable restrict
+° ඬ⃟🤖 #enable autoread*
+° ඬ⃟🤖 #disable autoread
+° ඬ⃟🤖 #enable autoread
+° ඬ⃟🤖 #addprems @tag
+° ඬ⃟🤖 #resetlimit
+° ඬ⃟🤖 #banlist
+° ඬ⃟🤖 #bcgc texto
+° ඬ⃟🤖 #bc texto
+° ඬ⃟🤖 #update
+° ඬ⃟🤖 #restart
+° ඬ⃟🤖 #backup
+° ඬ⃟🤖 #unbanchat
+° ඬ⃟🤖 #unban @tag
+° ඬ⃟🤖 #del
+
+<NOTA/>
+
+° Manda mensaje al propietario del Bot para solicitar que te agreguen a usuarios premium y poder usar el Bot sin limites 
+- El mensaje debe incluir el nombre del grupo en el que estas con el Bot
+
+「 𝙏͚͜͝͠𝙝͚͜͝͠𝙚͚͜͝͠ ͚͜͝͠𝙎͚͜͝͠𝙝͚͜͝͠𝙖͚͜͝͠𝙙͚͜͝͠𝙤͚͜͝͠𝙬͚͜͝͠ ͚͜͝͠𝘽͚͜͝͠𝙧͚͜͝͠𝙤͚͜͝͠𝙠͚͜͝͠𝙚͚͜͝͠𝙧͚͜͝͠𝙨͚͜͝͠ ͚͜͝͠-͚͜͝͠ ͚͜͝͠𝘽͚͜͝͠𝙤͚͜͝͠𝙩͚͜͝͠ ͚͜͝͠  」
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['menu']
+handler.tags = ['info']
+handler.command = /^menu2$/i
+
+module.exports = handler

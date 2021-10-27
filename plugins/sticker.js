386a398a6d35ab6beb359238287058ad81456c33
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-27 01:41:21 -0500

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -33,7 +33,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (stiker) await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
     })
-    else throw `Gagal${m.isGroup ? ', Error vuelva a intentarlo, recuerde responder a la imagen con #S o poner en la leyenda de la imagen el comando!' : ''}`
+    else throw `Lo siento.${m.isGroup ? ', Error vuelva a intentarlo, recuerde responder a la imagen con #S o poner en la leyenda de la imagen el comando!' : ''}`
   }
 }
 handler.help = ['stiker ', 'stiker <url>']

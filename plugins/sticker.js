d5e82906dcf4b29a931dcc48d2699150ecf951c8
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-27 01:31:35 -0500

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,47 +1,24 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
-const uploadFile = require('../lib/uploadFile')
-const uploadImage = require('../lib/uploadImage')
-let { webp2png } = require('../lib/webp2mp4')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+let handler  = async (m, { conn, args }) => {
   let stiker = false
   try {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
-    if (/webp|image|video/g.test(mime)) {
-      if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
+    if (/image|video/.test(mime)) {
       let img = await q.download()
-      if (!img) throw `Responde a una imagen/video/pegatina con el comando ${usedPrefix + command}`
-      let out
-      try {
-        if (/webp/g.test(mime)) out = await webp2png(img)
-        else if (/image/g.test(mime)) out = await uploadImage(img)
-        else if (/video/g.test(mime)) out = await uploadFile(img)
-        stiker = await sticker(false, out, global.packname, global.author)
-      } catch (e) {
-        console.error(e)
-        stiker = await sticker(img, false, global.packname, global.author)
-      }
-    } else if (args[0]) {
-      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-      else return m.reply('URL Invalida!')
-    }
-  } catch (e) {
-    console.error(e)
-    if (Buffer.isBuffer(e)) stiker = e
+      if (!img) throw 'Foto/Video tidak ditemukan'
+      stiker = await sticker(img, false, global.packname, global.author)
+    } else if (args[0]) stiker = await sticker(false, args[0], global.packname, global.author)
   } finally {
-    if (stiker) await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
     })
-    else throw `Error vuelva a intentarlo${m.isGroup ? ', envie la imagen con la leyenda #S o #Sticker!' : ''}`
+    else throw 'Conversion failed'
   }
 }
-handler.help = ['stiker ', 'stiker <url>']
+handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
 handler.tags = ['sticker']
 handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
 
 module.exports = handler
-
-const isUrl = (text) => {
-  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
-}

dc746684cd2878b8c1fe76e5ef6da9d330360451
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-28 01:48:38 -0500

    Create report.js

---
+++ plugins/report.js
@@ -0,0 +1,25 @@
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async(m, { conn, text }) => {
+    if (!text) return conn.reply(m.chat, 'Ingrese un reporte', m)
+    if (text > 300) return conn.reply(m.chat, 'Lo siento, el texto es demasiado largo, máximo 300 caracteres', m)
+    var nomor = m.sender
+    const teks1 = `*[REPORTE]*\nNumero: wa.me/${nomor.split("@s.whatsapp.net")[0]}\nMensaje: ${text}`
+    conn.sendMessage('19525220880@s.whatsapp.net', teks1, MessageType.text)
+    conn.reply(m.chat, '✔️ El Problema se ha informado al propietario del Bot ✔️', m)
+}
+handler.help = ['bug <reporte>', 'report <reporte>']
+handler.tags = ['info']
+handler.command = /^(bug|report)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler

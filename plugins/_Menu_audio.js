20be554eb707e6a1cefcb01454728d86e79f1fac
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-28 03:37:50 -0500

    Update _Menu_audio.js

--- plugins/_Menu_audio.js
+++ plugins/_Menu_audio.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { conn }) => {
-       conn.sendFile(m.chat, 'media/Menu audio.mp3', '', 'xd', '#help' m)
+       conn.sendFile(m.chat, 'media/Menu audio.mp3', '', 'xd', + text = '#help' m)
 }
-handler.command = /^(menu5)$/i
+handler.command = /^(menuaudio)$/i
 handler.fail = null
 handler.exp = 100
 module.exports = handler

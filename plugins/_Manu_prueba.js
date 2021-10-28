a80cb38e49e4d224647a7105307434deea32ba6e
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-28 03:29:18 -0500

    Create _Manu_prueba.js

---
+++ plugins/_Manu_prueba.js
@@ -0,0 +1,10 @@
+let handler  = async (m, { conn }) => {
+       conn.sendFile(m.chat, 'media/Buenos días.mp3', '', 'xd', m),
+let handler = async m => m.reply(`
+#help
+`.trim()) // Tambah sendiri kalo mau
+}
+handler.command = /^(menu)$/i
+handler.fail = null
+handler.exp = 100
+module.exports = handler

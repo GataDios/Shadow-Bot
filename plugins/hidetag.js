aa57efbdb8bed96f4a0865271b13b96dfe487c22
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-27 19:14:58 -0500

    Update hidetag.js

--- plugins/hidetag.js
+++ plugins/hidetag.js
@@ -1,12 +1,12 @@
 let { MessageType } = require('@adiwajshing/baileys')
 
-let handler = async (m, { conn, text, participants }) => {
-  let users = participants.map(u => u.jid)
-  conn.reply(m.chat, text, m, { contextInfo: { mentionedJid: users } })
+let handler = async (m, { conn, text }) => {
+  let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
+  conn.sendMessage(m.chat, text, MessageType.extendedText, { contextInfo: { mentionedJid: users } })
 }
-handler.help = ['pengumuman','announce','hidetag'].map(v => v + ' [teks]')
-handler.tags = ['group']
-handler.command = /^(pengumuman|announce|hiddentag|hidetag)$/i
+handler.help = ['pengumuman','hidetag'].map(v => 'o' + v + ' [teks]')
+handler.tags = ['owner']
+handler.command = /^(opengumuman|oannounce|ohiddentag|hidetag)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
@@ -17,6 +17,5 @@ handler.admin = false
 handler.botAdmin = false
 
 handler.fail = null
-handler.limit = false
 
 module.exports = handler

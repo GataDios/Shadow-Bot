86f97844d20d35926484f569f69e6276c1e9eaf7
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-27 22:25:56 -0500

    Update ara.js

--- plugins/ara.js
+++ plugins/ara.js
@@ -1,18 +1,18 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m) => {
-let ara = fs.readFileSync('./audio/Uh.mp3') 
-conn.sendMessage(m.chat, uh, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
+let ara = fs.readFileSync('./audio/Ara.mp3') 
+conn.sendMessage(m.chat, ara, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 }
 
-handler.customPrefix = ['uh'] 
+handler.customPrefix = ['Sonido1'] 
 handler.tags = ['audio'] 
-handler.command = /^(uh)$/i
+handler.command = /^(Sonido1)$/i
 
 handler.owner = false
 handler.mods = false 
 handler.premium = false
-handler.group = true 
-handler.private = true
+handler.group = false 
+handler.private = false
 
 module.exports = handler

9a5efaa267201b09c85497fc3239235e0843b000
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-27 23:02:32 -0500

    Update and rename baneado_mp3.js to _baneado_mp3.js

--- plugins/_baneado_mp3.js
+++ plugins/_baneado_mp3.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { conn }) => {
        conn.sendFile(m.chat, 'media/baneado.mp3', '', 'xd', m)
 }
-handler.command = /^(baneado)$/i
+handler.command = /^(baneadomp3)$/i
 handler.fail = null
 handler.exp = 100
 module.exports = handler

d13f4870706e0d8369eedb7bd5ec6690b144811a
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-28 03:32:34 -0500

    Update _Manu_prueba.js

--- plugins/_Manu_prueba.js
+++ plugins/_Manu_prueba.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { conn }) => {
        conn.sendFile(m.chat, 'media/Menu audio.mp3', '', 'xd', m),
      }
-  { 
+  {, 
 let handler = async m => m.reply(`
 #help
 `.trim()) // Tambah sendiri kalo mau 

5494aabeea42e7119c4a13b4f9dce87230ff0ee1
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-28 03:38:11 -0500

    Delete _Menu_audio.js

--- plugins/_Menu_audio.js
+++
@@ -1,7 +0,0 @@
-let handler  = async (m, { conn }) => {
-       conn.sendFile(m.chat, 'media/Menu audio.mp3', '', 'xd', + text = '#help' m)
-}
-handler.command = /^(menuaudio)$/i
-handler.fail = null
-handler.exp = 100
-module.exports = handler

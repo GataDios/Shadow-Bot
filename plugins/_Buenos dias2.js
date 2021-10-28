a5d4d2f15eb30c1b01f315b0d5866ae8990188db
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-27 23:50:13 -0500

    Update _Buenos dias2.js

--- plugins/_Buenos dias2.js
+++ plugins/_Buenos dias2.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-       conn.sendFile(m.chat, 'media/Buenos días 2.mp43', '', 'xd', m)
+       conn.sendFile(m.chat, 'media/Buenos días 2.mp3', '', 'xd', m)
 }
 handler.command = /^(buenosdias2)$/i
 handler.fail = null

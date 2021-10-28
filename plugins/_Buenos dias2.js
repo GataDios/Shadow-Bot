3d4129c19b1ecf41ed21731938a65505069e5703
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-27 23:33:13 -0500

    Create _Buenos dias2.js

---
+++ plugins/_Buenos dias2.js
@@ -0,0 +1,10 @@
+let handler  = async (m, { conn }) => {
+       conn.sendFile(m.chat, 'media/Buenos días 2.mp3', '', 'xd', m)
+}
+handler.command = /^(buenosdias2)$/i
+handler.fail = null
+handler.exp = 100
+module.exports = handler
+© 2021 GitHub, Inc.
+Terms
+Privacy

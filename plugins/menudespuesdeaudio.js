dc276a82d5d76aa17b416f88320bccec4cc3291e
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-28 03:12:29 -0500

    Delete menudespuesdeaudio.js

--- plugins/menudespuesdeaudio.js
+++
@@ -1,8 +0,0 @@
-let handler = async m => m.reply(`
-#help
-`.trim()) // Tambah sendiri kalo mau
-handler.help = ['menu']
-handler.tags = ['info']
-handler.command = /^menu$/i
-
-module.exports = handler

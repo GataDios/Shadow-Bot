c58c157d230849b466938379bb47efdaad248ccc
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-28 03:31:46 -0500

    Update _Manu_prueba.js

--- plugins/_Manu_prueba.js
+++ plugins/_Manu_prueba.js
@@ -1,9 +1,12 @@
 let handler  = async (m, { conn }) => {
-       conn.sendFile(m.chat, 'media/Buenos días.mp3', '', 'xd', m),
+       conn.sendFile(m.chat, 'media/Menu audio.mp3', '', 'xd', m),
+     }
+  { 
 let handler = async m => m.reply(`
 #help
-`.trim()) // Tambah sendiri kalo mau
+`.trim()) // Tambah sendiri kalo mau 
 }
+
 handler.command = /^(menu)$/i
 handler.fail = null
 handler.exp = 100

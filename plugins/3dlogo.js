8b0e4c0d98f22a5e1c4373c29a5e996c354d6b01
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-28 20:31:09 -0500

    Update 3dlogo.js

--- plugins/3dlogo.js
+++ plugins/3dlogo.js
@@ -1,17 +1,14 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ')
-  if (!args) throw 'Ingrese un texto'
-  m.reply('aguarde toy procesando')
- if (args.length < 1) return reply(`por favor use bien el comando ${prefix}text3d NeKosmic`)
-teks = `${body.slice(8)}`
-if (teks.length > 10) return teslagod.sendMessage(from, '[ ! ] 10 digitos maximo', text, {quoted: mek})
-buff = await getBuffer(`https://docs-jojo.herokuapp.com/api/text3d?text=${teks}`, {method: 'get'})
-teslagod.sendMessage(from, buff, image, {quoted: mek}) }
-
-handler.help = ['text3d'].map(v => v + ' <teks>')
-handler.tags = ['image']
-handler.command = /^(text3d)$/i
+  if (!args) throw 'Masukkan Parameter'
+  m.reply('*[❗] Wait,Tunggu Bentar Kak Sedang Dalam Proses...*')
+  let res = `http://lolhuman.herokuapp.com/api/photooxy3/bannerlol?apikey=31caf10e4a64e86c1a92bcba&text=${response}`
+  conn.sendFile(m.chat, res, 'nama.jpg', `Nih Mhank`, m, false)
+}
+handler.help = ['lolbanner'].map(v => v + ' <teks>')
+handler.tags = ['sticker']
+handler.command = /^(lolbanner)$/i
 handler.limit = false
 handler.register = false
 

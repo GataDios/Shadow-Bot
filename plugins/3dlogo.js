0f07adb971f9ae73b31c967ab14334778201f66d
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-28 20:26:45 -0500

    Update 3dlogo.js

--- plugins/3dlogo.js
+++ plugins/3dlogo.js
@@ -1,17 +1,17 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ')
-  if (!args) throw 'Masukkan Parameter'
+  if (!args) throw 'Ingrese un texto'
   m.reply('aguarde toy procesando')
  if (args.length < 1) return reply(`por favor use bien el comando ${prefix}text3d NeKosmic`)
 teks = `${body.slice(8)}`
 if (teks.length > 10) return teslagod.sendMessage(from, '[ ! ] 10 digitos maximo', text, {quoted: mek})
 buff = await getBuffer(`https://docs-jojo.herokuapp.com/api/text3d?text=${teks}`, {method: 'get'})
-teslagod.sendMessage(from, buff, image, {quoted: mek})
-}
-handler.help = ['logo'].map(v => v + ' <teks>')
+teslagod.sendMessage(from, buff, image, {quoted: mek}) }
+
+handler.help = ['text3d'].map(v => v + ' <teks>')
 handler.tags = ['image']
-handler.command = /^(logo)$/i
+handler.command = /^(text3d)$/i
 handler.limit = false
 handler.register = false
 

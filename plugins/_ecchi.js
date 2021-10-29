3bbd80e056a72942797c8783920a299fc27c7e10
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-29 11:26:09 -0500

    Update _ecchi.js

--- plugins/_ecchi.js
+++ plugins/_ecchi.js
@@ -1,6 +1,5 @@
 let axios = require('axios');
 let fetch = require('node-fetch')
-let neko = require('nekos.life')
 let Neko = new neko()
      let handler  = async (m, { conn, args }) => {
      json = (await axios.get('https://meme-api.herokuapp.com/gimme/ecchi')).data

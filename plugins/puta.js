a6ec3ce3779e2e7cbeba6f0adeb7067500fcaad7
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-29 11:48:14 -0500

    Create puta.js

---
+++ plugins/puta.js
@@ -0,0 +1,25 @@
+let handler = async (m, { conn, command, text }) => {
+  if (!text) throw `Quién es *${command.replace('puta', '').toUpperCase()}*`
+  conn.reply(m.chat, `
+*${text}* es *${Math.floor(Math.random() * 103890)}*% ${command.replace('puta', '').toUpperCase()}
+`.trim(), m, m.mentionedJid ? {
+    contextInfo: {
+      mentionedJid: m.mentionedJid
+    }
+  } : {})
+}
+handler.help = ['puta'].map(v => 'puta' + v + ' quien?')
+handler.tags = ['kerang']
+handler.command = /^(puta)/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler

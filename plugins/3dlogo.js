1f7174b37040b424b3e493bc7df970517d042ce5
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-28 20:20:04 -0500

    Create 3dlogo.js

---
+++ plugins/3dlogo.js
@@ -0,0 +1,19 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ')
+  if (!args) throw 'Masukkan Parameter'
+  m.reply('aguarde toy procesando')
+ if (args.length < 1) return reply(`por favor use bien el comando ${prefix}text3d NeKosmic`)
+teks = `${body.slice(8)}`
+if (teks.length > 10) return teslagod.sendMessage(from, '[ ! ] 10 digitos maximo', text, {quoted: mek})
+buff = await getBuffer(`https://docs-jojo.herokuapp.com/api/text3d?text=${teks}`, {method: 'get'})
+teslagod.sendMessage(from, buff, image, {quoted: mek})
+			     	break
+}
+handler.help = ['text3d'].map(v => v + ' <teks>')
+handler.tags = ['sticker']
+handler.command = /^(text3d)$/i
+handler.limit = false
+handler.register = false
+
+module.exports = handler

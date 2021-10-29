9cd01ee5a709ecb71181cff75924e3db4ce3260e
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-29 11:28:11 -0500

    Update _ecchi.js

--- plugins/_ecchi.js
+++ plugins/_ecchi.js
@@ -5,9 +5,9 @@ let Neko = new neko()
      json = (await axios.get('https://meme-api.herokuapp.com/gimme/ecchi')).data
    conn.sendFile(m.chat, json.url, 'ecchi.jpg', json.title, m, false)
 }
-handler.help = ['a']
+handler.help = ['ecchi']
 handler.tags = ['sange']
-handler.command = /^a$/i
+handler.command = /^(ecchi)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false

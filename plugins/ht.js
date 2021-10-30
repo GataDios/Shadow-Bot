382dd5b64c856db298e09969afe3909644402512
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-30 03:17:24 -0500

    Update ht.js

--- plugins/ht.js
+++ plugins/ht.js
@@ -14,9 +14,9 @@ let handler = async (m, { conn, text }) => {
     delete conn.hartatahta[m.chat]
   }
 }
-handler.help = ['tahta <teks>']
+handler.help = ['logo <teks>']
 handler.tags = ['tools']
-handler.command = /^((harta)?tahta)$/i
+handler.command = /^(logo)$/i
 handler.limit = false
 
 module.exports = handler

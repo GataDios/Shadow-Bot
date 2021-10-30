cea23b022b5532505ae9f23929cfa773388dc2e6
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-29 21:48:23 -0500

    Update puta.js

--- plugins/puta.js
+++ plugins/puta.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, command, text }) => {
   if (!text) throw `Quién es *${command.replace('how', '').toUpperCase()}*`
   conn.reply(m.chat, `
-*${text}* es *${Math.floor(Math.random() * 2000)}*% *${command.replace('how', '').toUpperCase()}* _Alguien quire sus servicios? xd_
+_*${text}* *es* *${Math.floor(Math.random() * 2000)}%* *${command.replace('how', '').toUpperCase()}* *Alguien quire sus servicios? xd*_
 `.trim(), m, m.mentionedJid ? {
     contextInfo: {
       mentionedJid: m.mentionedJid

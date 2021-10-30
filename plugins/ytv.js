79a02e51c1543b574c182786afba5637c1714bc1
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-30 05:17:32 -0500

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -8,16 +8,25 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
   let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+❒═════❬ YTMP4 ❭═════╾❒
+┬
+├‣*Nombre:* ${title}
+┴
+├‣*📂Tamaño:* ${filesizeF}
+┴
+├‣*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+┴
 `.trim(), m)
   let _thumb = {}
   try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
   catch (e) { }
   if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp4', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
+❒═════❬ YTMP4 ❭═════╾❒
+┬
+├‣*Nombre:* ${title}
+┴
+├‣*📂Tamaño:* ${filesizeF}
+┴
 `.trim(), m, false, {
   ..._thumb,
   asDocument: chat.useDocument

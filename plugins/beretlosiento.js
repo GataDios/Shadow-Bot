93973b356c372dc3f0dad16829b8985bf5e8b752
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-30 01:17:56 -0500

    Create beretlosiento.js

---
+++ plugins/beretlosiento.js
@@ -0,0 +1,10 @@
+let handler  = async (m, { conn }) => {
+       conn.sendFile(m.chat, 'media/beretlosiento.mp4', '', 'xd', m)
+}
+handler.command = /^(beretlosiento)$/i
+handler.fail = null
+handler.exp = 100
+module.exports = handler
+© 2021 GitHub, Inc.
+Terms
+Privac

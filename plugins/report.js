efe7a0b336bc2c363883d3efc6cb4efd5c52be38
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-30 09:42:24 -0500

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -7,7 +7,7 @@ let handler = async(m, { conn, text }) => {
     const teks1 = `*[REPORTE]*\nNumero: wa.me/${nomor.split("@s.whatsapp.net")[0]}\nMensaje: ${text}`
     conn.sendMessage('19525220880@s.whatsapp.net', teks1, MessageType.text)
     conn.sendMessage('5219996125657@s.whatsapp.net', teks1, MessageType.text)
-    conn.reply(m.chat, '*_✔️ El Problema se ha informado al propietario del Bot ✔️_*\n\n*_Si el Reporte es falso o "broma" puede llegar a ser bloqueado del uso del Bot_*', m)
+    conn.reply(m.chat, '*_✅ El problema se ha informado al propietario del Bot ✅_*\n\n*_⚠️Si el Reporte es falso o "broma" puede llegar a ser bloqueado del uso del Bot⚠️_*', m)
 }
 handler.help = ['bug <reporte>', 'report <reporte>']
 handler.tags = ['info']

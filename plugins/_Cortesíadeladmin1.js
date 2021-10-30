94d3ed2724cc1148627d87c1a331870e9fac7289
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-29 23:35:58 -0500

    Update _Cortesíadeladmin1.js

--- plugins/_Cortesíadeladmin1.js
+++ plugins/_Cortesíadeladmin1.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { conn }) => {
        conn.sendFile(m.chat, 'Cortesiadeladmin/cortesíadeladmin1.mp3', '', 'xd', m)
 }
-handler.command = /^(cortesíadeladmin1)$/i
+handler.command = /^(cortesiadeladmin1)$/i
 handler.fail = null
 handler.exp = 100
 module.exports = handler

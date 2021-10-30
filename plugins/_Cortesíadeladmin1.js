4cebe19fbd9e576f0819c6bdf9dd5877e241a673
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-29 23:20:26 -0500

    Update _Cortesíadeladmin1.js

--- plugins/_Cortesíadeladmin1.js
+++ plugins/_Cortesíadeladmin1.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-       conn.sendFile(m.chat, 'media/cortesíadeladmin1.mp4', '', 'xd', m)
+       conn.sendFile(m.chat, 'Cortesiadeladmin/cortesíadeladmin1.mp4', '', 'xd', m)
 }
 handler.command = /^(cortesíadeladmin1)$/i
 handler.fail = null

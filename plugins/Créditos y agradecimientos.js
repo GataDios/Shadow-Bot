aa8884931ac47c1bcb48d052ef3fc61e5d971487
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-30 10:08:00 -0500

    Update Créditos y agradecimientos.js

--- plugins/Créditos y agradecimientos.js
+++ plugins/Créditos y agradecimientos.js
@@ -17,6 +17,10 @@ Agradecemos a todas las personas que intervinieron en la edicion o aprobacion de
 
 Agradecemos a Confu y Samu que son los dueños de los canales usados en el tutorial
 
+Agradecemos a Emo del canal de Hacking Sin Fronteras por tomar algunos audios de su Bot 
+
+Agradecemos a los creadores de los siguientes Bots: 1)ShanBot 2)Bot Tiburon 3)fgv4 4)FX-SELF-BOT-v83.2 4)BAKA14
+
 *_「 𝙏͚͜͝͠𝙝͚͜͝͠𝙚͚͜͝͠ ͚͜͝͠𝙎͚͜͝͠𝙝͚͜͝͠𝙖͚͜͝͠𝙙͚͜͝͠𝙤͚͜͝͠𝙬͚͜͝͠ ͚͜͝͠𝘽͚͜͝͠𝙧͚͜͝͠𝙤͚͜͝͠𝙠͚͜͝͠𝙚͚͜͝͠𝙧͚͜͝͠𝙨͚͜͝͠ ͚͜͝͠-͚͜͝͠ ͚͜͝͠𝘽͚͜͝͠𝙤͚͜͝͠𝙩͚͜͝͠ ͚͜͝͠  」_*
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['creditosyagradecimientos']

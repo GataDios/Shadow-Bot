ccaeca65903ff248a04ecf825a92c547059935c8
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-30 00:18:36 -0500

    Delete _a.js

--- plugins/_a.js
+++
@@ -1,7 +0,0 @@
-let handler  = async (m, { conn }) => {
-       conn.sendFile(m.chat, 'media/a.mp4', '', 'xd', m)
-}
-handler.command = /^(a)$/i
-handler.fail = null
-handler.exp = 100
-module.exports = handler

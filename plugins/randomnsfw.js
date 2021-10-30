deb9e89f686d2ab7a5fd5bd772ec5bfe3553862c
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-30 01:38:59 -0500

    Update randomnsfw.js

--- plugins/randomnsfw.js
+++ plugins/randomnsfw.js
@@ -8,9 +8,10 @@ handler.tags = ['images']
 handler.command = /^(randomnsfw)$/i
 handler.owner = false
 handler.mods = false
-handler.premium = true
+handler.premium = false
 handler.group = false
 handler.private = false
+handler.register = true
 
 handler.admin = false
 handler.botAdmin = false

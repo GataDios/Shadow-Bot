d915715ac0cfafbf932a6cae12f21dc48533ea20
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-30 09:36:28 -0500

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, text }) => {
     if (text > 300) return conn.reply(m.chat, 'Lo siento, el texto es demasiado largo, máximo 300 caracteres', m)
     var nomor = m.sender
     const teks1 = `*[REPORTE]*\nNumero: wa.me/${nomor.split("@s.whatsapp.net")[0]}\nMensaje: ${text}`
-    conn.sendMessage('19525220880@s.whatsapp.net', teks1, MessageType.text)
+    conn.sendMessage('19525220880@s.whatsapp.net','5219996125657@s.whatsapp.net', teks1, MessageType.text)
     conn.reply(m.chat, '*_✔️ El Problema se ha informado al propietario del Bot ✔️_*\n\n*_Si el Reporte es falso o "broma" puede llegar a ser bloqueado del uso del Bot_*', m)
 }
 handler.help = ['bug <reporte>', 'report <reporte>']

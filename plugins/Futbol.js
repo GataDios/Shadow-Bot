be3de1badafe6e13fe91fca811994262a64478da
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-31 11:17:25 -0600

    Update Futbol.js

--- plugins/Futbol.js
+++ plugins/Futbol.js
@@ -5,7 +5,7 @@ let les = await axios.get('https://meme-api.herokuapp.com/gimme/futbol')
   }
 handler.help = ['futbol']
 handler.tags = ['images']
-handler.command = /^(futbol)$/i
+handler.command = /^(futbol|fútbol|football|\)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false

d2de0d97ce0b65bd161293ae346a41040667bbda
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-31 12:53:06 -0600

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -3,10 +3,10 @@ let Reg = /(.*)([.|])([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix }) {
   let user = global.DATABASE._data.users[m.sender]
   if (user.registered === true) throw `Ya estás registrado\nQuieres volver a registrarte? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
-  if (!Reg.test(text)) throw `Formato incorrecto\n*${usedPrefix}daftar nombre.edad ... Ejemplo: #daftar Shadow.18*`
+  if (!Reg.test(text)) throw `Formato incorrecto\n*${usedPrefix}daftar nombre.edad\nEjemplo: #daftar Shadow.18*`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw 'El nombre no puede estar vacío (Alphanumeric)'
-  if (!age) throw 'La edad no puede estar vacía (Angka)'
+  if (!name) throw 'El nombre no puede estar vacío'
+  if (!age) throw 'La edad no puede estar vacía'
   user.name = name
   user.age = parseInt(age)
   user.regTime = + new Date

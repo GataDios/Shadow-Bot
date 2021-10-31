fa16102618fd768345cfe7c4cf62abd8989b0604
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-30 21:48:00 -0500

    Update antilink.js

--- plugins/antilink.js
+++ plugins/antilink.js
@@ -7,9 +7,9 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    await m.reply(`*「 ANTI LINK 」*\n\nDetectado *${await this.getName(m.sender)}* has enviado un enlace al grupo!\n\nLo siento, serás expulsado de este grupo, adiós!`)
+    await m.reply(`*「 ANTI LINK 」*\n\nDetectado *${await this.getName(m.sender)}* *has enviado un enlace al grupo!*\n\n*Lo siento, serás expulsado de este grupo, adiós!*`)
     if (isAdmin) return m.reply('*Lo siento, eres administrador, no te expulsare..*')
-    if (!isBotAdmin) return m.reply('*El bot no es administrador, ¿cómo puede sacar a la gente? _-*')
+    if (!isBotAdmin) return m.reply('*_El bot no es administrador, ¿cómo puede sacar a la gente?_*')
     let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
     let isLinkThisGc = new RegExp(linkGC, 'i')
     let isgclink = isLinkThisGc.test(m.text)

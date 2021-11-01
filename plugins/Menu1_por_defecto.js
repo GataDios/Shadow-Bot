ce11122d6217168463db4d37701eb40afd8339d7
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-11-01 03:19:04 -0600

    Update Menu1_por_defecto.js

--- plugins/Menu1_por_defecto.js
+++ plugins/Menu1_por_defecto.js
@@ -276,35 +276,6 @@ _- Regalos de numero virtuales, pruebas de comandos nuevos, ayuda casi inmediata
 
 *IGNORAR LO SIGUIENTE*`,
 }
-let handler = async (m, { conn, usedPrefix: _p }) => {
-  try {
-    let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
-    let name = conn.getName(m.sender)
-    let d = new Date(new Date + 3600000)
-    let locale = 'id'
-    // d.getTimeZoneOffset()
-    // Offset -420 is 18.00
-    // Offset    0 is  0.00
-    // Offset  420 is  7.00
-    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
-    let week = d.toLocaleDateString(locale, { weekday: 'long' })
-    let date = d.toLocaleDateString(locale, {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    })
-    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    }).format(d)
-    let time = d.toLocaleTimeString(locale, {
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
-    })
-    let _uptime = process.uptime() * 1000
-    let _muptime
     if (process.send) {
       process.send('uptime')
       _muptime = await new Promise(resolve => {

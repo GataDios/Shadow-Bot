9503297d72109cb9e6cf7e6407fc9a7ab9d44e8c
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-11-01 11:01:37 -0600

    Update Oxxo_prueba.js

--- plugins/Oxxo_prueba.js
+++ plugins/Oxxo_prueba.js
@@ -1,13 +1,13 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m) => {
-let Oxxo = fs.readFileSync('/media/Oxxo.mp3') 
-conn.sendMessage(m.chat, Oxxo, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
+let oxxo = fs.readFileSync('/media/Oxxo.mp3') 
+conn.sendMessage(m.chat, oxxo, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 }
 
-handler.customPrefix = ['Prueba'] 
+handler.customPrefix = ['oxxx'] 
 handler.tags = ['audio'] 
-handler.command = /^(Prueba)$/i
+handler.command = /^(oxxo)$/i
 
 handler.owner = false
 handler.mods = false 

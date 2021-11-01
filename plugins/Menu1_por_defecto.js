61b2f183eb54bd7dbabd78d750749d20cf97611b
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-11-01 03:22:32 -0600

    Update Menu1_por_defecto.js

--- plugins/Menu1_por_defecto.js
+++ plugins/Menu1_por_defecto.js
@@ -285,18 +285,6 @@ _- Regalos de numero virtuales, pruebas de comandos nuevos, ayuda casi inmediata
     let body = conn.menu.body || defaultMenu.body
     let footer = conn.menu.footer || defaultMenu.footer
     let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + defaultMenu.after
-    let _text = [
-      before,
-      ...Object.keys(tags).map(tag => {
-        return header.replace(/%category/g, tags[tag]) + '\n' + [
-          ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
-            return menu.help.map(help => {
-              return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
-                .replace(/%islimit/g, menu.limit ? '(Limit)' : '')
-                .replace(/%isPremium/g, menu.premium ? '(Premium)' : '')
-                .trim()
-            }).join('\n')
-          }),
           footer
         ].join('\n')
       }),

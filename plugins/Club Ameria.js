248e17b3f487471ff5487f414c4e46d5dbb25a75
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-31 19:54:53 -0600

    Update Club Ameria.js

--- plugins/Club Ameria.js
+++ plugins/Club Ameria.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
  let handler = async(m, { conn }) => {
-let les = await axios.get('https://meme-api.herokuapp.com/gimme/Clubamerica')
+let les = await axios.get('https://meme-api.herokuapp.com/gimme/ClubAmerica')
             conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
   }
 handler.help = ['Clubamerica']

3ec10f85300442722ca209e687c4aad0fd993398
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-31 23:57:17 -0600

    Update Video2.js

--- plugins/Video2.js
+++ plugins/Video2.js
@@ -1,5 +1,7 @@
 let handler  = async (m, { conn }) => {
-       conn.sendFile(m.chat, 'media/video2.mp4', '', ''😏🔥, m)
+       conn.sendFile(m.chat, 'media/Espera2.mp3', '', 'xd', m)
+       conn.sendFile(m.chat, 'media/Musica de espera.mp3', '', 'xd', m)
+       conn.sendFile(m.chat, 'media/video2.mp4', '', '😏🔥', m)
 }
 handler.command = /^(video2)$/i
 handler.fail = null

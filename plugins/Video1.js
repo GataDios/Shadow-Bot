7e817b6d57e9c23dde1fabc2f3d30b1bd548c29e
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-10-31 23:34:01 -0600

    Update Video1.js

--- plugins/Video1.js
+++ plugins/Video1.js
@@ -1,4 +1,6 @@
 let handler  = async (m, { conn }) => {
+       conn.sendFile(m.chat, 'media/Espera2.mp3', '', 'xd, m)
+       conn.sendFile(m.chat, 'media/Musica de espera.mp3', '', 'xd, m)
        conn.sendFile(m.chat, 'media/video1.mp4', '', ''😏🔥, m)
 }
 handler.command = /^(video1)$/i

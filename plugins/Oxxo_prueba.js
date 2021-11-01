ebdabec4b2cbd8dd5b2335d8a91a81f8e140d57a
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-11-01 11:02:47 -0600

    Update Oxxo_prueba.js

--- plugins/Oxxo_prueba.js
+++ plugins/Oxxo_prueba.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m) => {
-let Oxxo = fs.readFileSync('/media/Oxxo.mp3') 
+let Oxxo = fs.readFileSync('./media/Oxxo.mp3') 
 conn.sendMessage(m.chat, Oxxo, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 }
 

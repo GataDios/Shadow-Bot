5acf1c286647b30f521ceedd6693662ed664f6c0
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-11-04 17:01:38 -0600

    Update Simsimi3.js

--- plugins/Simsimi3.js
+++ plugins/Simsimi3.js
@@ -1,12 +1,13 @@
+let { Presence } = require('@adiwajshing/baileys')
 let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-   response = args.join(' ')
+
+let handler  = async (m, { conn, args, usedPrefix, command }) => {
 	if (!args || !args[0]) return conn.reply(m.chat, `Formato incorrecto!\n\n*Ejemplpo* : _${usedPrefix + command} Hola simi_`, m)
 	let text = args.join` `
 	fetch("https://api.simsimi.net/v1/?text=" + encodeURIComponent(text) + "&lang=es")
   .then(res => res.json())
   .then(batch => {
-     conn.updatePresence(m.chat, Presence.composing)
+    conn.updatePresence(m.chat, Presence.composing)
   conn.reply(m.chat, `${batch.success}`, m)
   }) .catch(() => { conn.reply(m.chat, `_¡La función simi es errónea! Perdón :(_`, m) })
 }

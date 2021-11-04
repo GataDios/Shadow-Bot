87ca927d54c49973ffda2c7a0b5a03c3d09c71a7
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-11-03 20:06:53 -0600

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -7,7 +7,7 @@ let handler = async(m, { conn }) => {
   conn.sendFile(m.chat, json.url, '', 'Aqui tienes tu waifu', m)
 }
 handler.help = ['waifu']
-handler.tags = ['internet']
+handler.tags = ['General']
 handler.command = /^(waifu)$/i
 //MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS
 module.exports = handler

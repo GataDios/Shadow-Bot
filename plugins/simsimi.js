5f92d46bddab6fa366dbb36bc1f8d9b75546c628
TheShadowBrokers1 <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-11-04 17:13:50 -0600

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
   if (!text) throw 'Cuál es el texto?'
-  let res = await fetch("https://api.simsimi.net/v3/?text=" + encodeURIComponent(text) + "&lang=es")
+  let res = await fetch(global.API('bg', '/simi', { pesan: text }))
   let json = await res.json()
   if (json.status !== true) throw json
   m.reply(json.result.jawab)
